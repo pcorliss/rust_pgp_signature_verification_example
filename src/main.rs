f76485d9b89fa11bc2b387f22aef5700375a7483
Philip Corliss <pcorliss@50projects.com>
2020-06-10 19:36:26 +0000

    Public key loaded

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// #[macro_use]
+// extern crate pgp;
+
+use std::fs::File;
+// use std::io::{Cursor, Read};
+
+// use pgp::composed::{Deserializable, Message, SignedPublicKey, SignedSecretKey};
+use pgp::composed::{SignedPublicKey, Deserializable};
+// use pgp::types::KeyTrait;
+
 fn main() {
     println!("Hello, world!");
+    let res = SignedPublicKey::from_armor_single(
+        File::open("./bigdevbox_public.asc").unwrap(),
+    );
+    println!("Result: {:?}", res);
 }
